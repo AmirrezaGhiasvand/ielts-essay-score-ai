4b81e95e3c6f4afead8e51e8310902e226effd30
Mojtaba Alizadeh <moj.dazalih@gmail.com>
2026-06-19 21:52:50 +0330

    add header to a seperate component

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -17,7 +17,11 @@ export default function RootLayout({
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+Arabic:wght@400;500;600;700&display=swap"
           rel="stylesheet"
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import ModelSelector from "@/app/components/ModelSelector";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -9,7 +8,6 @@ import {
   Loader2,
   RotateCcw,
   Clock,
-  ChevronDown,
   ChevronDownIcon,
   ClipboardPasteIcon,
 } from "lucide-react";
@@ -20,8 +18,6 @@ import BandGauge from "@/app/components/BandGauge";
 import CriterionCard from "@/app/components/CriterionCard";
 import Chat from "@/app/components/Chat";
 import ReactMarkdown from "react-markdown";
-import { FaGithub } from "react-icons/fa";
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import ErrorHighlightedEssay from "@/app/components/ErrorHighlightedEssay";
 import { getSampleEssay } from "@/app/lib/api";
 import { Shuffle } from "lucide-react";
@@ -30,8 +26,8 @@ import {
   DisclosureButton,
   DisclosurePanel,
 } from "@headlessui/react";
-import { useRef, useEffect } from "react";
 import TextType from "./components/TextType";
+import MainHeader from "./MainHeader";
 
 // -------- Form schema --------
 
@@ -210,66 +206,20 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-[#0F1117]" dir={langInfo!.dir}>
       {/* ---- Header ---- */}
-      <header className="border-b border-[#2A2D3A] bg-[#0F1117] px-3 py-2 flex items-center justify-between sticky top-0 z-20 backdrop-blur-sm">
-        <div className="flex items-center gap-3">
-          <a
-            className="w-9 h-9 bg-[#C8102E] hover:bg-[#C8102E]/70 rounded flex items-center justify-center shrink-0 transition-colors duration-150"
-            href="https://github.com/AmirrezaGhiasvand/ielts-essay-score-ai"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaGithub className="w-6 h-6 cursor-pointer" />
-          </a>
-          <div>
-            <h1 className="text-md font-semibold text-slate-100">{t.title}</h1>
-            <p className="text-[11px] text-slate-500">{t.subtitle}</p>
-          </div>
-        </div>
-
-        {/* ---- Controls ---- */}
-        <div className="flex items-center gap-4">
-          <ModelSelector onModelChange={handleModelChange} />
-          <Menu>
-            <MenuButton
-              onClick={() => setLangOpen(!langOpen)}
-              className="flex items-center gap-2 text-sm text-slate-300 hover:text-white bg-[#1A1D27] border border-[#2A2D3A] hover:border-[#C8102E] rounded-lg px-4 py-2 transition-colors font-medium focus:outline-none focus:ring-0 focus-visible:outline-none"
-            >
-              {LANGUAGES.find((l) => l.code === language)?.label}
-              <ChevronDown size={11} />
-            </MenuButton>
-            <MenuItems
-              anchor={{ to: "bottom end", gap: "8px" }}
-              transition
-              className="p-2 bg-[#1A1D27] border border-[#2A2D3A] rounded-xl shadow-2xl z-30 w-42 overflow-hidden focus:outline-none focus:ring-0 focus-visible:outline-none origin-top transition duration-200 ease-in-out data-closed:scale-95 data-closed:opacity-0"
-            >
-              {LANGUAGES.map((lang) => (
-                <MenuItem key={lang.code}>
-                  <button
-                    onClick={() => {
-                      localStorage.setItem("lang", lang.code);
-                      setLanguage(lang.code);
-                      setLangOpen(false);
-                    }}
-                    className={`w-full text-center px-3 py-2 my-0.5 text-xs transition-colors rounded-2xl ${
-                      language === lang.code
-                        ? "text-[#C8102E] bg-[#C8102E]/10 font-semibold"
-                        : "text-slate-400 hover:text-slate-200 hover:bg-[#0F1117]"
-                    }`}
-                  >
-                    {lang.label}
-                  </button>
-                </MenuItem>
-              ))}
-            </MenuItems>
-          </Menu>
-        </div>
-      </header>
+      <MainHeader
+        title={t.title}
+        subtitle={t.subtitle}
+        handleModelChange={handleModelChange}
+        langOpen={langOpen}
+        setLangOpen={setLangOpen}
+        setLanguage={setLanguage}
+        language={language}
+        LANGUAGES={LANGUAGES}
+      />
 
       {/* ---- Main ---- */}
       <main className="w-full px-3 py-2">
-        {/* ---- Left: Form ---- */}
-
-        {/* ---- Right: Results + Chat ---- */}
+        {/* Loading */}
         {loading ? (
           <div className="min-h-[calc(100vh-75px)] flex justify-center items-center">
             <TextType
